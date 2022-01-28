18ea9deabddb284b2cf5562a3cf2b8b4166140ca
Kanan <43238830+KenanBoyukkishiyev1997@users.noreply.github.com>
2022-01-28 14:37:25 +0400

    show

--- assets/js/iland.js
+++ assets/js/iland.js
@@ -83,7 +83,6 @@ function loaderGltf() {
 manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
 
 	console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-
     $("#gltfShow").LoadingOverlay("show", {
         background: 'red',
         image: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000"><circle r="80" cx="500" cy="90"/><circle r="80" cx="500" cy="910"/><circle r="80" cx="90" cy="500"/><circle r="80" cx="910" cy="500"/><circle r="80" cx="212" cy="212"/><circle r="80" cx="788" cy="212"/><circle r="80" cx="212" cy="788"/><circle r="80" cx="788" cy="788"/></svg>`,
@@ -92,9 +91,8 @@ manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
     });
 
     // Hide it after 3 seconds
-    setTimeout(function(){
         $("#gltfShow").LoadingOverlay("hide");
-    }, 3000);
+  
 };
 
 manager.onLoad = function ( ) {
